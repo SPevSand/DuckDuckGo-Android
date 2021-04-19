5b9f67da4018bba0b0b8844090728e93d3979e22
Dmitrij Nikiforov <YM9e8sz7XgPD7Uct@yandex.ru>
2021-04-19 14:26:33 +0300

    quick fix

--- app/src/main/java/com/duckduckgo/app/browser/DuckDuckGoWebView.kt
+++ app/src/main/java/com/duckduckgo/app/browser/DuckDuckGoWebView.kt
@@ -19,8 +19,6 @@ package com.duckduckgo.app.browser
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
-import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputConnection
@@ -28,7 +26,6 @@ import android.webkit.WebView
 import androidx.core.view.NestedScrollingChild
 import androidx.core.view.NestedScrollingChildHelper
 import androidx.core.view.ViewCompat
-import kotlin.math.abs
 
 /**
  * WebView subclass which allows the WebView to
